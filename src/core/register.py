cfeafe1febfb227bde6d29198d0ac25afbf3e1eb
Your Name <you@example.com>
2025-11-28 22:49:16 +0500

    feat: app register

--- src/core/register.py
+++ src/core/register.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.config.settings import settings
-from src.api.routers import v1
-from src.database.postgres import create_table, close_pooled_connections
+from src.controller.routers import v1
+from src.database.connection import create_table, close_pooled_connections
 
 
 @asynccontextmanager
@@ -14,12 +15,12 @@ async def register_init(app: FastAPI):
 
 def register_app() -> FastAPI:
     app = FastAPI(
-        title=settings.TITLE,
-        version=settings.VERSION,
-        description=settings.DESCRIPTION,
-        openapi_prefix=settings.OPENAPI_PREFIX,
-        docs_url=settings.DOCS_URL,
-        openapi_url=settings.OPENAPI_URL,
+        title="Warehouse Management System",
+        # version=settings.VERSION,
+        # description=settings.DESCRIPTION,
+        # openapi_prefix=settings.OPENAPI_PREFIX,
+        # docs_url=settings.DOCS_URL,
+        # openapi_url=settings.OPENAPI_URL,
         lifespan=register_init,
     )
     register_middleware(app)
@@ -28,16 +29,13 @@ def register_app() -> FastAPI:
 
 
 def register_middleware(app: FastAPI):
-    if settings.MIDDLEWARE_CORS:
-        from fastapi.middleware.cors import CORSMiddleware
-
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=["*"],
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 def register_router(app: FastAPI):
